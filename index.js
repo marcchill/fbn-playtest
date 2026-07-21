f730f3e9aa86d3ecca8cc1d5e44f735f4a3839ef
Mark Childerhose <mark.childerhose@gmail.com>
2026-07-21 20:56:16 +0200

    fbn web build 0.3.1-sweep14 (sweep 14 wayfinding/FTUE + tweak rounds; first cache-busted deploy)

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ const Preloader = /** @constructor */ function () { // eslint-disable-line no-un
 			loaded: 0,
 			done: false,
 		};
-		return fetch(file).then(function (response) {
+		return fetch(file + '?v=0.3.1-sweep14').then(function (response) {
 			if (!response.ok) {
 				return Promise.reject(new Error(`Failed loading file '${file}'`));
 			}
